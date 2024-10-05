b06d54631d131a1ec16a8acb9aa2911925a5e833
root <root@SHRUTI>
2024-10-06 01:39:05 +0530

    Initial project setup

---
+++ api/index.ts
@@ -0,0 +1,9 @@
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+
+const app = new Hono()
+app.get('/', (c) => c.text('Hello Node.js!'))
+
+serve(app,()=>{
+    console.log("Running at http://localhost:3000");
+})
